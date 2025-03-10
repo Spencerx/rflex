0a854cd56528a69ddb0c3c57e03f83a0b942442c
suma <suman.orega.warukatta@gmail.com>
2025-03-10 22:06:52 +0900

    Update example1

--- examples/example1/src/main.rs
+++ examples/example1/src/main.rs
@@ -3,8 +3,9 @@ mod test {
 }
 
 fn main() {
-    let s = "abc a A ABC abC_def";
-    //let s = "abc !".to_string();  // match unmatch
+    let s = "abc a A ABC abC_def \"string\" ";
+    //let s = "abc !".to_string();  // unmatch
+    //let s = "\" \n \"".to_string();  // unmatch
     let mut lex = test::Lexer::new(&s, test::SpaceCounter::new());
     loop {
         let res = lex.yylex();
